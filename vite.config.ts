bbae14a0a032c06cb7bb66fd274e4110d62e40e9
rt-rocket <108962288+rt-rocket@users.noreply.github.com>
2023-12-22 12:19:20 -0600

    Change base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'portfolio',
+  base: 'https://rtrinh760.github.io/portfolio/',
   plugins: [react()],
 })
