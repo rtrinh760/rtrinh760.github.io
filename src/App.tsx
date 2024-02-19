6b212b4333cb6188259cce838fef5cce5f5297af
rt-rocket <108962288+rt-rocket@users.noreply.github.com>
2024-02-19 16:11:13 -0500

    Update link styling to hover

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
                   <a
                     href="https://linkedin.com/in/rtrinh760"
                     target="_blank"
-                    className="flex items-center space-x-2 transition duration-200 underline decoration-transparent hover:decoration-indigo-400 underline-offset-2 decoration-2"
+                    className="flex items-center space-x-2 transition duration-200 underline decoration-transparent hover:decoration-indigo-400 underline-offset-4 decoration-2"
                   >
                     <svg className="fill-white" width="24" height="24">
                       <path d="M19 0H5a5 5 0 0 0-5 5v14a5 5 0 0 0 5 5h14a5 5 0 0 0 5-5V5a5 5 0 0 0-5-5zM8 19H5V8h3v11zM6.5 6.732c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zM20 19h-3v-5.604c0-3.368-4-3.113-4 0V19h-3V8h3v1.765c1.396-2.586 7-2.777 7 2.476V19z" />
@@ -24,7 +24,7 @@ function App() {
                   <a
                     href="https://github.com/rtrinh760"
                     target="_blank"
-                    className="flex items-center space-x-2 transition duration-200 underline decoration-transparent hover:decoration-indigo-400 underline-offset-2 decoration-2"
+                    className="flex items-center space-x-2 transition duration-200 underline decoration-transparent hover:decoration-indigo-400 underline-offset-4 decoration-2"
                   >
                     <svg className="fill-white" width="24" height="24">
                       <path d="M12 0C5.374 0 0 5.373 0 12c0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23A11.509 11.509 0 0 1 12 5.803c1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576C20.566 21.797 24 17.3 24 12c0-6.627-5.373-12-12-12z" />
@@ -35,7 +35,7 @@ function App() {
                 <li className="px-1 py-4 sm:px-4">
                   <a
                     href="mailto:richardtrinh452@gmail.com"
-                    className="flex items-center space-x-2 transition duration-200 underline decoration-transparent hover:decoration-indigo-400 underline-offset-2 decoration-2"
+                    className="flex items-center space-x-2 transition duration-200 underline decoration-transparent hover:decoration-indigo-400 underline-offset-4 decoration-2"
                   >
                     <svg className="fill-white" width="24" height="24">
                       <path d="M0 3v18h24V3H0zm21.518 2L12 12.713 2.482 5h19.036zM2 19V7.183l10 8.104 10-8.104V19H2z" />
@@ -48,7 +48,7 @@ function App() {
                     href="https://rtrinh760.github.io/Richard_Trinh_resume.pdf"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="flex items-center space-x-2 transition duration-200 underline decoration-transparent hover:decoration-indigo-400 underline-offset-2 decoration-2"
+                    className="flex items-center space-x-2 transition duration-200 underline decoration-transparent hover:decoration-indigo-400 underline-offset-4 decoration-2"
                   >
                     <svg
                       className="fill-white"
@@ -81,23 +81,19 @@ function App() {
                   <a
                     href="https://www.terracon.com/"
                     target="_blank"
-                    className="transition duration-200 ease-in-out underline underline-offset-2 decoration-indigo-400 hover:decoration-white decoration-2 transform"
+                    className="transition duration-200 underline decoration-transparent hover:decoration-indigo-400 underline-offset-4 decoration-2"
                   >
-                    Terracon
+                    Terracon - Software Engineer Intern (Summer 2023)
                   </a>
-                  {" - "}
-                  Software Engineer Intern (Summer 2023)
                 </p>
                 <p>
                   <a
                     href="https://www.cs.colby.edu/"
                     target="_blank"
-                    className="transition duration-200 ease-in-out underline underline-offset-2 decoration-indigo-400 hover:decoration-white decoration-2 transform"
+                    className="transition duration-200 underline decoration-transparent hover:decoration-indigo-400 underline-offset-4 decoration-2"
                   >
-                    Colby College
+                    Colby College - CS Teaching Assistant (Fall 2023 - Present)
                   </a>
-                  {" - "}
-                  CS Teaching Assistant (Fall 2023 - Present)
                 </p>
               </div>
               <h2 className="font-bold py-6 text-xl md:text-2xl">Projects</h2>
@@ -108,13 +104,11 @@ function App() {
                       href="https://github.com/rtrinh760/scholar_ninja"
                       target="_blank"
                       rel="noopener noreferrer"
-                      className="transition duration-200 ease-in-out underline underline-offset-2 decoration-indigo-400 hover:decoration-white decoration-2 transform"
+                      className="transition duration-200 underline decoration-transparent hover:decoration-indigo-400 underline-offset-4 decoration-2"
                     >
-                      Scholar Ninja
+                      Scholar Ninja - Personalized scholarships finder (React
+                      Native, Flask, Firebase)
                     </a>
-                    {" - "}
-                    Personalized scholarships finder (React Native, Flask,
-                    Firebase)
                   </p>
                 </div>
                 <div>
@@ -123,13 +117,11 @@ function App() {
                       href="https://github.com/rtrinh760/vidsnap"
                       target="_blank"
                       rel="noopener noreferrer"
-                      className="transition duration-200 ease-in-out underline underline-offset-2 decoration-indigo-400 hover:decoration-white decoration-2 transform"
+                      className="transition duration-200 underline decoration-transparent hover:decoration-indigo-400 underline-offset-4 decoration-2"
                     >
-                      Vidsnap
+                      Vidsnap - AI video summarizer (Next.js, React, TypeScript,
+                      YouTube API)
                     </a>
-                    {" - "}
-                    AI video summarizer (Next.js, React, TypeScript, YouTube
-                    API)
                   </p>
                 </div>
                 <div>
@@ -138,12 +130,11 @@ function App() {
                       href="https://github.com/rtrinh760/scholarai"
                       target="_blank"
                       rel="noopener noreferrer"
-                      className="transition duration-200 ease-in-out underline underline-offset-2 decoration-indigo-400 hover:decoration-white decoration-2 transform"
+                      className="transition duration-200 underline decoration-transparent hover:decoration-indigo-400 underline-offset-4 decoration-2"
                     >
-                      ScholarAI
+                      ScholarAI - PDF document search (Next.js, React,
+                      TypeScript, PostgreSQL)
                     </a>
-                    {" - "}
-                    PDF document search (Next.js, React, TypeScript, PostgreSQL)
                   </p>
                 </div>
                 <div>
@@ -152,12 +143,11 @@ function App() {
                       href="https://github.com/rtrinh760/atoll-segmentation"
                       target="_blank"
                       rel="noopener noreferrer"
-                      className="transition duration-200 ease-in-out underline underline-offset-2 decoration-indigo-400 hover:decoration-white decoration-2 transform"
+                      className="transition duration-200 underline decoration-transparent hover:decoration-indigo-400 underline-offset-4 decoration-2"
                     >
-                      Coral Reef Annotator
+                      Coral Reef Annotator - Atoll segmentation ML model
+                      (OpenCV, TensorFlow, Keras)
                     </a>
-                    {" - "}
-                    Atoll segmentation ML model (OpenCV, TensorFlow, Keras)
                   </p>
                 </div>
                 <div>
@@ -166,12 +156,11 @@ function App() {
                       href="https://github.com/rtrinh760/pycoMouse"
                       target="_blank"
                       rel="noopener noreferrer"
-                      className="transition duration-200 ease-in-out underline underline-offset-2 decoration-indigo-400 hover:decoration-white decoration-2 transform"
+                      className="transition duration-200 underline decoration-transparent hover:decoration-indigo-400 underline-offset-4 decoration-2"
                     >
-                      Micromouse
+                      Micromouse - Maze-traversing robotic mouse (Python,
+                      CircuitPython)
                     </a>
-                    {" - "}
-                    Maze-traversing robotic mouse (Python, CircuitPython)
                   </p>
                 </div>
                 <div>
@@ -180,13 +169,11 @@ function App() {
                       href="https://rtrinh760.github.io/pathfinding-visualizer/"
                       target="_blank"
                       rel="noopener noreferrer"
-                      className="transition duration-200 ease-in-out underline underline-offset-2 decoration-indigo-400 hover:decoration-white decoration-2 transform"
+                      className="transition duration-200 underline decoration-transparent hover:decoration-indigo-400 underline-offset-4 decoration-2"
                     >
-                      Pathfinding Visualizer
+                      Pathfinding Visualizer - Grid simulation of various
+                      traversal algorithms (React, TypeScript)
                     </a>
-                    {" - "}
-                    Grid simulation of various traversal algorithms (React,
-                    TypeScript)
                   </p>
                 </div>
                 <div>
@@ -195,13 +182,11 @@ function App() {
                       href="https://github.com/rtrinh760/sudoku-solver"
                       target="_blank"
                       rel="noopener noreferrer"
-                      className="transition duration-200 ease-in-out underline underline-offset-2 decoration-indigo-400 hover:decoration-white decoration-2 transform"
+                      className="transition duration-200 underline decoration-transparent hover:decoration-indigo-400 underline-offset-4 decoration-2"
                     >
-                      Sudoku Solver
+                      Sudoku Solver - Simulation of backtracking algorithms via
+                      Sudoku Solving (Java)
                     </a>
-                    {" - "}
-                    Simulation of backtracking algorithms via Sudoku Solving
-                    (Java)
                   </p>
                 </div>
               </div>
