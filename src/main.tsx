0f85c3abf6c3f2fe9905ad33dd7598e1d0c9088c
rt-rocket <108962288+rt-rocket@users.noreply.github.com>
2024-04-12 17:14:12 -0400

    GH Pages issue: rm React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Root from './routes/Root.tsx'
+import App from './App.tsx'
 import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import ErrorPage from './error-page.tsx';
-import Resume from './routes/Resume.tsx';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "/resume",
-    element: <Resume />,
-    errorElement: <ErrorPage />
-  }
-])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
 )
