6133f171170ffae94c91db8458b2c9f1b23ea65b
rt-rocket <108962288+rt-rocket@users.noreply.github.com>
2024-04-12 16:59:52 -0400

    Add error and resume pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './routes/App.tsx'
+import Root from './routes/Root.tsx'
 import './index.css'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import ErrorPage from './error-page.tsx';
+import Resume from './routes/Resume.tsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />
+    element: <Root />,
+    errorElement: <ErrorPage />
   },
   {
     path: "/resume",
-    element: <App />
+    element: <Resume />,
+    errorElement: <ErrorPage />
   }
 ])
 
